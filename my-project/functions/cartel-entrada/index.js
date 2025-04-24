0b7527bdfb52e57b0973ef8566d0ed3e3021e480
matbutom <163034603+matbutom@users.noreply.github.com>
2025-04-24 17:47:15 -0400

    agregar logos y carpeeta assets

--- my-project/functions/cartel-entrada/index.js
+++ my-project/functions/cartel-entrada/index.js
@@ -2,30 +2,43 @@ export const handler = ({ inputs, mechanic, sketch }) => {
   const { textLine1, textLine2, textLine3, color1 } = inputs;
   const width = 1080;
   const height = 1350;
+  let qrCodeImage;
+
+  sketch.preload = () => {
+    qrCodeImage = sketch.loadImage('qr_lid_ig.png'); // Precargamos la imagen
+  };
 
   sketch.setup = () => {
     sketch.createCanvas(width, height);
   };
 
   sketch.draw = () => {
-    sketch.background("#FFFFFF"); // Fondo blanco para el poster
+    sketch.background("#FFFFFF");
     sketch.noStroke();
     sketch.fill(color1);
-    sketch.textAlign(sketch.CENTER, sketch.TOP); // Alinear texto al centro horizontalmente y arriba verticalmente
+    sketch.textAlign(sketch.CENTER, sketch.TOP);
 
-    const margin = width * 0.1; // 10% de margen horizontal
-    const lineHeight = height / 15; // Espacio vertical aproximado por línea
+    const margin = width * 0.1;
+    const lineHeight = height / 15;
 
-    sketch.textSize(lineHeight * 1.2); // Tamaño de fuente un poco mayor que el espacio entre líneas
+    sketch.textSize(lineHeight * 1.2);
     sketch.textStyle(sketch.BOLD);
-    sketch.text(textLine1, margin, height * 0.1, width - 2 * margin); // Título cerca de la parte superior
+    sketch.text(textLine1, margin, height * 0.1, width - 2 * margin);
 
     sketch.textSize(lineHeight);
     sketch.textStyle(sketch.NORMAL);
-    sketch.text(textLine2, margin, height * 0.35, width - 2 * margin); // Subtítulo debajo del título
+    sketch.text(textLine2, margin, height * 0.35, width - 2 * margin);
 
     sketch.textSize(lineHeight * 0.8);
-    sketch.text(textLine3, margin, height * 0.55, width - 2 * margin); // Cuerpo de texto más abajo
+    sketch.text(textLine3, margin, height * 0.55, width - 2 * margin);
+
+    // Dibujar el código QR arriba a la derecha de 150x150 px
+    if (qrCodeImage) {
+      const qrCodeSize = 150; // Tamaño del código QR
+      const qrCodeX = width - qrCodeSize - 50; // Posición X: Derecha con margen de 50px
+      const qrCodeY = 50; // Posición Y: Arriba con margen de 50px
+      sketch.image(qrCodeImage, qrCodeX, qrCodeY, qrCodeSize, qrCodeSize);
+    }
 
     mechanic.done();
   };
@@ -55,7 +68,7 @@ export const inputs = {
   }
 };
 
-export const presets = {}; // Eliminamos los presets por ahora
+export const presets = {};
 
 export const settings = {
   engine: require("@mechanic-design/engine-p5")
